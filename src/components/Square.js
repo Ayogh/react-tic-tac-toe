95a69e8fb6af948cfb75fa43370abcf6c2b89503
Ayogh <aayogh@ahoo.com>
2020-12-21 13:44:23 -0500

    Fill square with X when clicked

---
+++ src/components/Square.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// Fill Square with X when clicked, i.e.,
+// g
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+export default Square
